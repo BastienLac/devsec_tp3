bc2d2e3ff2fdecd3c48bdd21792e311cc6bdf828
Fenyx <64859587+steven-van@users.noreply.github.com>
2025-02-25 14:00:47 +0100

    feat: task table

--- db.sql
+++ db.sql
@@ -7,3 +7,12 @@ CREATE TABLE users (
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE task (
+    id INT AUTO AUTO_INCREMENT PRIMARY KEY,
+    text VARCHAR(255) NOT NULL,
+    priority ENUM('critical', 'high', 'medium', 'low') NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
